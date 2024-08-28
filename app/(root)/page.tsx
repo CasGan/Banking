5de59a77b8c7c625ed80d21b13807170a4319f02
Cassandra Ganska <89425242+CasGan@users.noreply.github.com>
2024-08-27 22:43:42 -0600

    initial sign up with appwrite

---
+++ app/(root)/page.tsx
@@ -0,0 +1,35 @@
+import RightSidebar from "@/components/RightSidebar";
+import TotalBalanceBox from "@/components/TotalBalanceBox";
+import HeaderBox from "@/components/ui/HeaderBox";
+import React from "react";
+
+const Home = () => {
+  const loggedIn = {
+    firstName: "Marke",
+    lastName: "Rich",
+    email: "Rich1@email.com",
+  };
+  return (
+    <section className="home">
+      <div className="home-content">
+        <header className="home-header">
+          <HeaderBox
+            type="greeting"
+            title="Welcome"
+            user={loggedIn?.firstName || "Guest"}
+            subtext="Access and Manage your Account and Trasactions efficiently."
+          />
+          <TotalBalanceBox
+            accounts={[]}
+            totalBanks={1}
+            totalCurrentBalance={1250.35}
+          />
+        </header>
+        RECENT TRANSACTIONS
+      </div>
+      <RightSidebar user={loggedIn} transactions={[]} banks={[{ currentBalance: 1250.32}, { currentBalance: 2290.32}]} />
+    </section>
+  );
+};
+
+export default Home;
